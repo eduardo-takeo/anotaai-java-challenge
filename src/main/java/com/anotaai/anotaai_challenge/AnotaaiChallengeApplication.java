1c0033053f124d7730a6ca0f25499ceb208664e5
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2025-05-27 23:04:54 -0300

    feat: start with spring initializer

---
+++ src/main/java/com/anotaai/anotaai_challenge/AnotaaiChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.anotaai.anotaai_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotaaiChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotaaiChallengeApplication.class, args);
+	}
+
+}
